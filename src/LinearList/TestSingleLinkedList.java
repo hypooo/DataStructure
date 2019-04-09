1fc11ac827f3d9737335b6ce96fdce6495bd9d5e
hypooo <hypo1264776347@gmail.com>
2019-04-09 21:45:48 +0800

    by hypo

---
+++ src/LinearList/TestSingleLinkedList.java
@@ -0,0 +1,34 @@
+package LinearList;
+
+/**
+ * @author:HYPO
+ * @date:2019/4/9 17:10
+ * @description:
+ */
+public class TestSingleLinkedList {
+    public static void main(String[] args) {
+        Integer[] values = {1,2,3,4,5};
+        Integer[] values2 = {4,5,6,7,8};
+        SingleLinkedList<Integer> list = new SingleLinkedList<Integer>(values);
+        SingleLinkedList<Integer> list2 = new SingleLinkedList<Integer>(values2);
+
+//        System.out.println(list.toString());
+//        System.out.println("单链表的长度为："+list.getSize());
+//        System.out.println("是否为空："+list.isEmpty());
+
+//        list.addNode(3,7);
+//        System.out.println(list.toString());
+
+//        list.deleteNode(4);
+//        System.out.println(list.toString());
+
+//        list.updateNode(3,111);
+//        System.out.println(list.toString());
+
+//        list.searchNode(3);
+
+        System.out.println(list.getDifference(list2));
+        System.out.println(list.getTheSame(list2));
+
+    }
+}
